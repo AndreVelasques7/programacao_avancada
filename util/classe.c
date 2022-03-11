bd02ae42764bfd70e38e88045c9bf328f5afc32b
André Velasques <34725160+AndreVelasques7@users.noreply.github.com>
2022-03-11 20:05:06 -0300

    Create classe.c

---
+++ util/classe.c
@@ -0,0 +1,18 @@
+#ifndef nome_a_sua_escolha
+#define nome_a_sua_escolha
+
+class Classe {
+public:
+    Classe();
+    Classe(int atributo);
+
+    ~Classe();
+
+    void metodo_classe(int atributo);
+
+private:
+    int _atributo;
+};
+
+
+#endif
